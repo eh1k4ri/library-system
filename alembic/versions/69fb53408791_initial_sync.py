f61f3b3033fbbe3fb7300df9d9c91bc9c9413779
Esther Hikari <esther.nunes@sou.inteli.edu.br>
2026-01-04 23:06:32 -0300

    FIX: lint

--- alembic/versions/69fb53408791_initial_sync.py
+++ alembic/versions/69fb53408791_initial_sync.py
@@ -8,7 +8,6 @@ Create Date: 2026-01-03 23:52:08.041374
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
--- app/models/loan_event.py
+++ app/models/loan_event.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, DateTime, String
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.db.session import Base
