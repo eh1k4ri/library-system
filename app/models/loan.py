42b3a76b49fd7e51509f3021f7578acc0ba98c7c
Esther Hikari <esther.nunes@sou.inteli.edu.br>
2026-01-04 22:32:19 -0300

    FIX: lint

--- app/models/loan.py
+++ app/models/loan.py
@@ -1,6 +1,6 @@
 import uuid
 from sqlalchemy import Column, Integer, ForeignKey, DateTime, Float
-from sqlalchemy.types import Uuid as SQLAlchemyUuid  # <--- A CORREÇÃO
+from sqlalchemy.types import Uuid as SQLAlchemyUuid
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.db.session import Base
