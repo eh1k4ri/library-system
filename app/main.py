2c9ca8254a31473e0c2f4c4435d8990dd218b55f
Esther Hikari <esther.nunes@sou.inteli.edu.br>
2026-01-05 22:55:56 -0300

    UPDATE: logger to main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from app.api.routers import users, books, healthcheck, loans
+from app.core.logger import configure_logging
+from app.core.middlewares import log_requests_middleware
 
+configure_logging()
 app = FastAPI(
     title="Library System API",
     docs_url="/docs",
 )
-
+app.middleware("http")(log_requests_middleware)
 app.include_router(healthcheck.router, prefix="", tags=["System"])
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
